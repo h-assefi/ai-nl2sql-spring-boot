fd0b55e8f7880ce913e5c9f54ac4f0915473d073
Hossein Assefi <hossein@Hosseins-MacBook-Air.local>
2026-01-03 12:57:08 +0330

    initial commit

---
+++ nl2sql/src/main/java/com/hossein/nl2sql/Nl2sqlApplication.java
@@ -0,0 +1,13 @@
+package com.hossein.nl2sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nl2sqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nl2sqlApplication.class, args);
+	}
+
+}
---
+++ nl2sql/src/test/java/com/hossein/nl2sql/Nl2sqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hossein.nl2sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nl2sqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
